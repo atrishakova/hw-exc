8dc68d8ebec512f1aec24dbaea85e970c2c598db
Alexandra Trishakova <svmail97@gmail.com>
2026-01-31 14:43:20 +0500

    try/catch

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,38 @@
 import ru.netology.PasswordChecker.PasswordChecker;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите мин. длину пароля: ");
-        int minLength = scanner.nextInt();
-
-        System.out.print("Введите макс. допустимое количество повторений символа подряд: ");
-        int maxCountRepeats = scanner.nextInt();
-
         PasswordChecker passwordChecker = new PasswordChecker();
 
-        passwordChecker.setMaxRepeats(maxCountRepeats);
-        passwordChecker.setMinLength(minLength);
+        try {
+            System.out.print("Введите мин. длину пароля: ");
+            int minLength = scanner.nextInt();
 
-        while (true) {
-            System.out.print("Введите пароль или end: ");
-            String input = scanner.next();
+            System.out.print("Введите макс. допустимое количество повторений символа подряд: ");
+            int maxCountRepeats = scanner.nextInt();
 
-            if (input.equals("end")) {
-                System.out.println("Программа завершена");
-                break;
+            passwordChecker.setMaxRepeats(maxCountRepeats);
+            passwordChecker.setMinLength(minLength);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            while (true) {
+                System.out.print("Введите пароль или end: ");
+                String input = scanner.next();
+
+                if (input.equals("end")) {
+                    System.out.println("Программа завершена");
+                    break;
+                }
+
+                System.out.println(passwordChecker.verify(input) ? "Подходит!" : "Не подходит!");
             }
-
-            System.out.println(passwordChecker.verify(input) ? "Подходит!" : "Не подходит!");
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
         }
 
         scanner.close();
